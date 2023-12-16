a617bc0834fe87754ff6a9ce2865acf0adb18a07
Ponte, Nadia <deponte.work@gmail.com>
2023-12-16 00:17:58 +0000

    Writes the current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@
 dayjs.extend(window.dayjs_plugin_customParseFormat);
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 
+// Writes the current day
+var today = dayjs();
+console.log(today);
+console.log(today.format('dddd, MMMM Do'));
+$('#currentDay').text(today.format('dddd, MMMM Do'));
+
