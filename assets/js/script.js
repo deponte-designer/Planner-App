370a2660b2ae14f9c8ba833209adf5d9a86f106c
Ponte, Nadia <deponte.work@gmail.com>
2023-12-16 14:58:44 +1400

    getting the actual hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 
 // Writes the current day
 var today = dayjs();
-console.log(today);
+// console.log(today);
 console.log(today.format('dddd, MMMM Do'));
 $('#currentDay').text(today.format('dddd, MMMM Do'));
+
+// Gets the actual hour
+const nowHour = dayjs().format('hA'); // HA = 0-23 AM/PM  // hA = 0-12 AM/PM > 9AM, 10AM, ... 12PM, 1PM, ...
+console.log(nowHour);
 
